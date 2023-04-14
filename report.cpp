dd1677275f77af0d24ed4127118f1dcd8dbc2e15
dasrhan devani <devanidarshan333@gmail.com>
2023-04-14 17:11:23 +0530

    report

--- report.cpp
+++ report.cpp
@@ -3,12 +3,20 @@ using namespace std;
 class REPORT
 {
     private:
-    char adno[5];
+    char adno;
     char name[20];
     float marks [5];
+    float total;
     float average;
-    void GETAVG();
-    
+    void GETAVG()
+    {
+    float sum=0;
+    for(int i=1;i<=5;i++)
+       {
+        sum = sum + marks[i];
+       }
+       average=sum/5;
+    }
     public:
     void READINFO()
     {
@@ -16,17 +24,25 @@ class REPORT
         cin>>adno;
         cout<<"Enter name : ";
         cin>>name;
-        cout<<"Enter marks: ";
-        cin>>marks;
+        cout<<"Enter 5 subject of marks: "<<endl;
+        for(int i=1;i<=5;i++)
+        {
+            cin>>marks[i];
+        }
          
-         GETAVG();
+        GETAVG();
     } 
     void DISPLAYINFO()
     {
         cout<<"admission  no: "<<adno<<endl;
         cout<<"name: "<<name<<endl;
-        cout<<"marks: "<<marks<<endl;
-        cout<<"average marks: "<<average<<endl;
+        cout<<"5 sub marks is : ";
+        for(int i=1;i<=5;i++)
+        {
+            cout<<marks[i]<<" ";
+        }
+
+        cout<<"\naverage marks: "<<average<<endl;
     }
 
 };
