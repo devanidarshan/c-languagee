8eb088e81d241c36c97a0aee17e2118b6d2fdd89
dasrhan devani <devanidarshan333@gmail.com>
2023-04-24 14:24:28 +0530

    singleinheri

---
+++ singleinheri.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std; 
+class base
+{
+    protected :
+    int n;
+     
+    public:
+    void setdata(int x)
+    {
+        n=x;
+    }
+
+};
+
+class derived : public base
+{
+    public:
+    void getdata()
+    {
+        cout<<"Value of n: "<<n<<endl;
+    }
+};
+
+int main()
+{
+    derived d;
+    d.setdata(101);
+    d.getdata();
+}
+
