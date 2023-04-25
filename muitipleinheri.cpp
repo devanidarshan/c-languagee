9164a61782312eec32014bbf0cf1d8d5b3bf67c1
dasrhan devani <devanidarshan333@gmail.com>
2023-04-25 14:20:17 +0530

    multipleinheri

---
+++ muitipleinheri.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std; 
+class base1
+{
+    protected :
+    int n;
+     
+    public:
+    void setn(int x)
+    {
+       n=x;
+    }
+
+};
+
+class base2
+{
+    protected :
+    int m;
+     
+    public:
+    void setm(int x)
+    {
+       m=x;
+    }
+
+};
+class derived : public base1 ,public base2
+{
+    public:
+    void mul()
+    {
+        cout<<"Multiplication of m and n is:  "<<m*n<<endl;
+    }
+    
+};
+
+int main()
+{
+    derived d;
+    d.setn(10);
+    d.setm(50);
+    d.mul();
+}
